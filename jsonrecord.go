b4f7e396b0b18459b8d812ba50bddfe246807ff7
James Bronder <36022278+jbronder@users.noreply.github.com>
2024-08-11 18:01:20 -0700

    added GeoJSON struct types for umarshalling USGS response

---
+++ jsonrecord.go
@@ -0,0 +1,57 @@
+package main
+
+type USGSResponse struct {
+	Type     string    `json:"type"`
+	Meta     Metadata  `json:"metadata"`
+	Bbox     []float64 `json:"bbox"`
+	Features []Feature `json:"features"`
+}
+
+type Metadata struct {
+	Generated int64  `json:"generated"`
+	Url       string `json:"url"`
+	Title     string `json:"title"`
+	Api       string `json:"api"`
+	Count     int    `json:"count"`
+	Status    int    `json:"status"`
+}
+
+type Feature struct {
+	Type  string     `json:"type"`
+	Props Properties `json:"properties"`
+	Geo   Geometry   `json:"geometry"`
+	Id    string     `json:"id"`
+}
+
+type Properties struct {
+	Mag     float64 `json:"mag"`
+	Place   string  `json:"place"`
+	Time    int64   `json:"time"`
+	Updated int64   `json:"updated"`
+	Tz      int     `json:"tz"`
+	Url     string  `json:"url"`
+	Detail  string  `json:"detail"`
+	Felt    int     `json:"felt"`
+	Cdi     float64 `json:"cdi"`
+	Mmi     float64 `json:"mmi"`
+	Alert   string `json:"alert"`
+	Status  string  `json:"status"`
+	Tsunami int     `json:"tsunami"`
+	Sig     int     `json:"sig"`
+	Net     string  `json:"net"`
+	Code    string  `json:"code"`
+	Ids     string  `json:"ids"`
+	Sources string  `json:"sources"`
+	Types   string  `json:"types"`
+	Nst     int     `json:"nst"`
+	Dmin    float64 `json:"dmin"`
+	Rms     float64 `json:"rms"`
+	Gap     float64 `json:"gap"`
+	MagType string  `json:"magType"`
+	Type    string  `json:"type"`
+}
+
+type Geometry struct {
+	Type        string    `json:"type"`
+	Coordinates []float64 `json:"coordinates"`
+}
