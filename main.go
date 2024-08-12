363c7a8239bb9aa9c449fc03af40d3225291737b
James Bronder <36022278+jbronder@users.noreply.github.com>
2024-08-12 10:22:27 -0700

    feat: endpoint call retrieves JSON over CSV
    In order to use Go's JSON umarshaling, JSON is called as the endpoint
    and then formatted to standard output. Also, moved CSV processing
    functions to the csvrecord.go file.

--- main.go
+++ main.go
@@ -3,14 +3,11 @@
 package main
 
 import (
-	"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 	"time"
 )
 
@@ -33,8 +30,6 @@ func main() {
 	opts := &Options{*formatFlag, *timeFlag, *magFlag}
 	fileName := extractFileName(opts)
 
-	// fmt.Println("extracting", ENDPOINT+fileName)
-
 	response, err := http.Get(ENDPOINT + fileName)
 	if err != nil {
 		log.Fatal(err)
@@ -45,22 +40,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//fmt.Println(string(bContent))
-	// using for parsing to human readable format
-	// bContent, err := os.ReadFile("eq-out.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	// flag switches from command line args to
 	switch opts.format {
 	case "human":
-		csvRecords := createCSVRecords(bContent)
-		humanize(csvRecords)
+		features := extractFeatures(bContent)
+		stdoutFeatures(features)
 	case "csv":
 		fmt.Println(string(bContent))
 	case "json":
-		fmt.Println("json todo")
+		fmt.Println(string(bContent))
 	default:
 		fmt.Println("todo")
 	}
@@ -99,50 +87,18 @@ func extractFileName(cmdOpt *Options) string {
 		fmt.Println("todo") // is this needed?
 	}
 
-	csvfile := fmt.Sprintf("/%s_%s.csv", magRange, timeRange)
-	return csvfile
-}
-
-// humanize outputs a human readable table that fits within a terminal.
-// It outputs fields that a person curious about earthquake occurrences may be
-// commonly interested in such as: Date, time, magnitude, where it occurred, and
-// the latitude and longitudinal coordinates of the location.
-func humanize(recs []*CSVRecord) {
-	//Date       Time     Mag   Place                            Lat    Long
-	fmt.Printf("%s %18s %-40s %-4s %4s\n", "Date-Time", "Mag", "Place", "Lat", "Long")
-	for i := range recs {
-		dateTimeVal := processTime(recs[i].time)
-		dateTimeStr := stringifyDateTime(dateTimeVal)
-		fmt.Fprintf(os.Stdout, "%s %3.2f %-40s %4.2f %4.2f\n",
-			dateTimeStr, recs[i].mag, recs[i].place, recs[i].latitude, recs[i].longitude)
-	}
-}
-
-// createCSVRecords processes the CSV data received from the server and creates
-// individual records out of each line CSV data
-func createCSVRecords(resContent []byte) []*CSVRecord {
-	strContent := string(resContent)
-	csvReader := csv.NewReader(strings.NewReader(strContent))
-
-	var records []*CSVRecord
-
-	for {
-		record, err := csvReader.Read()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
-		}
-
-		if !strings.Contains(record[0], "time") {
-			csvRec := NewRecord(record)
-			records = append(records, csvRec)
-		}
+	var fileSuffix string
+	switch cmdOpt.format {
+	case "human":
+		fileSuffix = "geojson"
+	case "csv":
+		fileSuffix = "csv"
+	case "json":
+		fileSuffix = "geojson"
 	}
 
-	return records
+	endpointFile := fmt.Sprintf("/%s_%s.%s", magRange, timeRange, fileSuffix)
+	return endpointFile
 }
 
 func processTime(timeStr string) time.Time {
