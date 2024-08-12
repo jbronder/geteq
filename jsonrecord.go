f7724ab158e3d0c467738ab2d215c9a43fe5fe87
James Bronder <36022278+jbronder@users.noreply.github.com>
2024-08-12 10:11:02 -0700

    feat: add extraction functions of GeoJSON features
    To retrieve real-time earthquake data, the GeoJSON data should be used
    over the CSV data, in order to use Go's JSON unmarshaling capabilities.

--- jsonrecord.go
+++ jsonrecord.go
@@ -1,10 +1,18 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
 type USGSResponse struct {
 	Type     string    `json:"type"`
 	Meta     Metadata  `json:"metadata"`
 	Bbox     []float64 `json:"bbox"`
-	Features []Feature `json:"features"`
+	Features `json:"features"`
 }
 
 type Metadata struct {
@@ -23,6 +31,8 @@ type Feature struct {
 	Id    string     `json:"id"`
 }
 
+type Features []Feature
+
 type Properties struct {
 	Mag     float64 `json:"mag"`
 	Place   string  `json:"place"`
@@ -34,7 +44,7 @@ type Properties struct {
 	Felt    int     `json:"felt"`
 	Cdi     float64 `json:"cdi"`
 	Mmi     float64 `json:"mmi"`
-	Alert   string `json:"alert"`
+	Alert   string  `json:"alert"`
 	Status  string  `json:"status"`
 	Tsunami int     `json:"tsunami"`
 	Sig     int     `json:"sig"`
@@ -54,4 +64,28 @@ type Properties struct {
 type Geometry struct {
 	Type        string    `json:"type"`
 	Coordinates []float64 `json:"coordinates"`
+}
+
+func extractFeatures(res []byte) Features {
+	var usgsRes USGSResponse
+	err := json.Unmarshal(res, &usgsRes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f := make(Features, 0, len(usgsRes.Features))
+	for _, v := range usgsRes.Features {
+		f = append(f, v)
+	}
+	return f
+}
+
+func stdoutFeatures(features Features) {
+	fmt.Printf("%s %18s %-40s %-4s %4s\n", "Date-Time", "Mag", "Place", "Lat", "Long")
+	for _, f := range features {
+		dateTimeVal := time.Unix(f.Props.Time, 0)
+		dateTimeStr := stringifyDateTime(dateTimeVal)
+		fmt.Fprintf(os.Stdout, "%s %3.2f %-40s %4.2f %4.2f\n",
+			dateTimeStr, f.Props.Mag, f.Props.Place, f.Geo.Coordinates[1], f.Geo.Coordinates[0])
+	}
 }
